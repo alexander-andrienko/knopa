a2961a9b22cdf0ff94dade2ef393d0a41e46376c
Alexander Andrienko <alexandera@interlink-ua.com>
2015-02-05 19:45:32 +0200

    add default model

--- com/in6k/src/web/js/app.js
+++ com/in6k/src/web/js/app.js
@@ -1,6 +1,10 @@
-App = angular.module('app',['ngRoute']);
+App = angular.module('app',['ngRoute', 'mgcrea.ngStrap', 'ngSanitize']);
 
 App.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        controller: 'homeCtrl',
+        templateUrl: 'html/home.tmpl.html'
+    });
     $routeProvider.when('/home', {
         controller: 'homeCtrl',
         templateUrl: 'html/home.tmpl.html'
@@ -9,5 +13,74 @@ App.config(function($routeProvider) {
         controller: 'UserCtrl',
         templateUrl: 'html/user.tmpl.html'
     });
+    $routeProvider.when('/activity/create', {
+        controller: 'activityCtrl',
+        templateUrl: 'html/activity.tmpl.html'
+    });
     $routeProvider.otherwise({redirectTo: '/home'});
+});
+
+App.factory('activityService', function($http) {
+    var _createActivity = function(name) {
+        return name
+    };
+    var _getActivity = function(id) {
+        return {
+            name: 'Belka',
+            container: {
+                name: '',
+                status: 'inprogress',
+                children: [
+                    {
+                        name: 'blue room',
+                        children: [
+                            {
+                                name: 'Mykolay',
+                                isComplete: true
+                            },
+                            {
+                                name: 'Alexey',
+                                isComplete: true
+                            },
+                            {
+                                name: 'Alexandr',
+                                isComplete: true
+                            },
+                            {
+                                name: 'Oleksandr',
+                                isComplete: true
+                            }
+                        ],
+                        status: 'complete'
+                    },
+                    {
+                        name: 'yellow room',
+                        children: [
+                            {
+                                name: 'Mykolay',
+                                isComplete: true
+                            },
+                            {
+                                name: 'Alexey',
+                                isComplete: true
+                            },
+                            {
+                                name: 'Alexandr',
+                                isComplete: false
+                            },
+                            {
+                                name: 'Oleksandr',
+                                isComplete: false
+                            }
+                        ],
+                        status: 'inprogress'
+                    }
+                ]
+            }
+        }
+    };
+    return {
+        createActivity: _createActivity,
+        getActivity: _getActivity
+    }
 });
--- com/in6k/src/web/js/controller/homeCtrl.js
+++ com/in6k/src/web/js/controller/homeCtrl.js
@@ -1,3 +1,32 @@
-App.controller('homeCtrl', function($scope) {
+App.controller('homeCtrl', function($scope, activityService, $location) {
+    $scope.createActivity = function() {
+        $location.url("/activity/create")
+    }
 
+});
+
+App.controller('activityCtrl', function($scope, $route, activityService) {
+    $scope.init = function() {
+        if (!$route.current.params.id) {
+            $scope.isCreate = true;
+            $scope.activity = {
+                container: {
+                    children: []
+                }
+            }
+        }
+        else {
+            $scope.activity = activityService.getActivity($route.current.params.id);
+        }
+    };
+
+    $scope.addContainer = function() {};
+
+    $scope.addStep = function(container) {
+        if (!container.newStep || container.newStep.length <= 0)
+            return;
+
+        var step = {name: container.newStep, children: []};
+        container.children.push(step);
+    };
 });
